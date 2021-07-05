fbe929b30ff5e5e6a686887db68d71c7b777dfe5
3albert3 <83572023+3albert3@users.noreply.github.com>
2021-07-05 14:59:42 +0200

    Update settings.py

--- pGea/settings.py
+++ pGea/settings.py
@@ -149,5 +149,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "caraculitodemar@gmail.com"
-EMAIL_HOST_PASSWORD = 'Nadieescomootro1'
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ''
